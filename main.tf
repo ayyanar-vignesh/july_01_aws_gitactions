8888abb1f20d70579b990495543ffc11b75653ff
ayyanar-vignesh <ayyanar.2481996@gmail.com>
2025-07-01 23:09:36 +0530

    formatted successfuly

--- main.tf
+++ main.tf
@@ -79,11 +79,11 @@ resource "aws_iam_role_policy_attachment" "lambda_s3_full_access" {
 # Lambda Function
 # ------------------------------
 resource "aws_lambda_function" "lambda_function" {
-  function_name = var.lambda_function_name
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.12"
-  role          = aws_iam_role.lambda_exec_role.arn
-  filename      = "lambda.zip"
+  function_name    = var.lambda_function_name
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.12"
+  role             = aws_iam_role.lambda_exec_role.arn
+  filename         = "lambda.zip"
   source_code_hash = filebase64sha256("lambda.zip")
 
   depends_on = [
